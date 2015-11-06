d97ffc4a1294cbafc3f78947d53c95efd86cf41c
harrison <harrison.chiu.136@gmail.com>
2015-11-06 06:01:42 -0500

    commenting and refactoring

--- uncross_the_lines.js
+++ uncross_the_lines.js
@@ -108,21 +108,26 @@ var success = false;
 
 // make a circle, and add it to the set of all circles
 var makeCircle = function(x, y) {
+
+    // make the circle
     var circle = draw.circle(diameter)
         .center(x, y)
         .front();
 
-    // the circles attached to this one
+    // give the circle a place to store its friends
+    // friends = circles to which it's connected
     circle.friends = [];
 
+    // add the circle to the master list
     circles.add(circle);
     return circle;
 
 }
 
-// move circle (relative to circle itself)
+// move circle
 var move = function(circle, dx, dy) {
 
+    // get new coordinates
     var nx = circle.cx() + dx;
     var ny = circle.cy() + dy;
 
@@ -148,12 +153,17 @@ var move = function(circle, dx, dy) {
 // connect two circles
 var connect = function (c1, c2) {
 
+    // if they're not connected...
     if (!connected(c1, c2)) {
+
+        // make the line
         var line =  draw.line(c1.cx(), c1.cy(),
                               c2.cx(), c2.cy())
             .stroke("#555555")
             .after(c1)
             .after(c2);
+
+        // update state variables
         lines[c1 + c2] = line;
         crossed[line] = true;
 
@@ -169,23 +179,31 @@ var connect = function (c1, c2) {
 // disconnect two circles
 var disconnect = function (c1, c2) {
 
+    // if they're connected
     if (connected(c1, c2)) {
-        // get the line
+
+        // get the line and remove it from the master list
         var line = lines[c1 + c2];
         lines.splice(c1 + c2, 1);
         if (!line) {
             line = lines[c2 + c1];
             lines.splice(c2 + c1, 1);
         }
+
+        // remove the line from our list of crossings
         crossed.splice(line, 1);
         line.plot(0, 0, 0, 0);
+
+        // remove line
+        // TODO: make sure lines are removed from memory
+        // right now i think .remove() actually just hides them
         line.remove();
         
+        // remove friends from circles
         var i1 = c1.friends.indexOf(c2);
         if (i1 >= 0) {
             c1.friends.splice(i1, 1);
         }
-
         var i2 = c2.friends.indexOf(c1);
         if (i2 >= 0) {
             c2.friends.splice(i2, 1);
@@ -193,18 +211,24 @@ var disconnect = function (c1, c2) {
     }
 }
 
+// check to see if circles are connected
 var connected = function (c1, c2) {
     
+    // if c2 is c1's friend
     for (var i in c1.friends) {
         if (c1.friends[i] === c2) {
             return true;
         }
     }
+    // if c1 is c2's friend
     for (var i in c2.friends) {
         if (c2.friends[i] === c1) {
             return true;
         }
     }
+    // the second check might not be necessary,
+    // but safe programming is good programming
+    // also it's only like ~5 more accesses, at worst
     return false;
 }
 
@@ -221,73 +245,123 @@ var drawLine = function(c1, c2) {
     line.plot(c1.cx(), c1.cy(),
               c2.cx(), c2.cy());
 
+    // update all the lines
+    checkAllLines();
+    recolorLines();
+}
+
+// check all the lines
+// runtime is a little scary!
+var checkAllLines = function () {
+
+    // for all our lines
     for (var i in lines) {
+        checkLine(lines[i]);
+    }
+}
 
-        var line1 = lines[i];
-        crossed[line1] = false;
+// check to see if a line intersects any lines
+var checkLine = function (line) {
 
-        for (var j in lines) {
+    crossed[line] = false;
+    // for all our lines
+    for (var i in lines) {
+        var line2 = lines[i]
 
-            var line2 = lines[j]
-
-            if (linesIntersect(line1, line2)) {
-                crossed[line1] = true;
-                break;
-            }
+        if (linesIntersect(line, line2)) {
+            crossed[line] = true;
+            return true;
         }
     }
+    return false;
+}
 
+// recolor all the lines properly
+// note: relies on crossed[],
+// ideally call after checking lines
+var recolorLines = function () {
+
+    // for all our lines
     for (var i in lines) {
+        // set its stroke to either red or green
         lines[i].stroke( crossed[lines[i]] ? "red" : "green" );
     }
 }
 
-// check if lines intersect
+// check if two lines intersect
 // super huge thanks to:
 // http://jeffe.cs.illinois.edu/teaching/373/notes/x06-sweepline.pdf
 var linesIntersect = function (l1, l2) {
 
-    var x0, y0, x1, y1, x2, y2, x3, y3;
-    
-    x0 = l1.array().value[0][0];
-    y0 = l1.array().value[0][1];
+    // only check if bboxes intersect
+    if (bboxIntersect(l1, l2)) {
 
-    x1 = l1.array().value[1][0];
-    y1 = l1.array().value[1][1];
+        // a bunch of accessing and terminology fixing
+        var x0, y0, x1, y1, x2, y2, x3, y3;
+        
+        x0 = l1.array().value[0][0];
+        y0 = l1.array().value[0][1];
 
-    x2 = l2.array().value[0][0];
-    y2 = l2.array().value[0][1];
+        x1 = l1.array().value[1][0];
+        y1 = l1.array().value[1][1];
 
-    x3 = l2.array().value[1][0];
-    y3 = l2.array().value[1][1];
+        x2 = l2.array().value[0][0];
+        y2 = l2.array().value[0][1];
 
-    var samePoint = function (x0, y0, x1, y1) {
-        return (Math.abs(x0 - x1) < diameter / 1000 &&
-                Math.abs(y0 - y1) < diameter / 1000);
+        x3 = l2.array().value[1][0];
+        y3 = l2.array().value[1][1];
+
+        // if the lines share points, pretend they don't intersect
+        // since they're on the same circle
+        return (!samePoint(x0, y0, x2, y2) &&
+                !samePoint(x0, y0, x3, y3) &&
+                !samePoint(x1, y1, x2, y2) &&
+                !samePoint(x1, y1, x3, y3)) &&
+            // check the orientations of bunches of points
+            // see PDF linked above for details
+            // or ask a math person
+            (CCW(x0, y0, x2, y2, x3, y3) !==
+             CCW(x1, y1, x2, y2, x3, y3) &&
+             CCW(x2, y2, x0, y0, x1, y1) !==
+             CCW(x3, y3, x0, y0, x1, y1));
     }
-
-    // share endpoint test
-    return (!samePoint(x0, y0, x2, y2) &&
-            !samePoint(x0, y0, x3, y3) &&
-            !samePoint(x1, y1, x2, y2) &&
-            !samePoint(x1, y1, x3, y3)) &&
-        // counterclockwise test
-        (CCW(x0, y0, x2, y2, x3, y3) !==
-         CCW(x1, y1, x2, y2, x3, y3) &&
-         CCW(x2, y2, x0, y0, x1, y1) !==
-         CCW(x3, y3, x0, y0, x1, y1));
-    
+    else {
+        return false;
+    }
 }
 
+// check to see if two points are basically the same
+var samePoint = function (x0, y0, x1, y1) {
+    return (Math.abs(x0 - x1) < diameter / 100 &&
+            Math.abs(y0 - y1) < diameter / 100);
+}
+
+// check to see if three points are counterclockwise
 var CCW = function (x0, y0, x1, y1, x2, y2) {
     return 0 > Math.sign(crossProduct(x1 - x0, y1 - y0,
                                       x2 - x0, y2 - y0));
 }
 
+// vectors yay!
 var crossProduct = function (x0, y0, x1, y1) {
     return x0 * y1 - x1 * y0;
 }
 
+// check if the game is currently solved
+var didWeWin = function () {
+    success = true;
+    // if any line is crossed, we didn't solve it
+    for (var i in crossed) {
+        if (crossed[i]) {
+            success = false;
+            break;
+        }
+    }
+    // if we won, make it green
+    // if not, it's red
+    // remember, rgb
+    background.fill( success ? "#eeffee" : "#ffeeee" );
+}
 // ================================
 
 // ALL ABOUT SELECTION
@@ -300,11 +374,16 @@ var selected = function(circle) {
 
 // add circle to selection
 var add = function(circle) {
+
+    // if it's not already selected
+    // surprisingly, svg.js doesn't have this check
     if (!selected(circle)) {
         
         selection.add(circle);
         circle.fill("red").front();
 
+        // make all the circle's friends blue
+        // it looks really cool!
         for (var i in circle.friends) {
             var friend = circle.friends[i];
             if (!selected(friend)) {
@@ -318,11 +397,15 @@ var add = function(circle) {
 
 // remove circle from selection
 var remove = function(circle) {
+
+    // only remove if it's selected
     if (selected(circle)) {
 
         selection.remove(circle);
         circle.fill("black");
 
+        // uncolor this circle's friends
+        // unless they're selected, in which case they stay red
         for (var i in circle.friends) {
             var friend = circle.friends[i];
             if (!selected(friend)) {
@@ -350,7 +433,7 @@ var empty = function () {
     }
 
     return selection;
-    // using remove inside of each fucks everything up
+    // using .remove inside of .each fucks everything up
 }
 
 // update circle under mouse
@@ -395,7 +478,7 @@ var drawBox = function () {
 
 var bboxIntersect = function (shape1, shape2) {
 
-    // extract values
+    // extract values, for corners
     var b1 = shape1.bbox();
     var b2 = shape2.bbox();
     
@@ -419,8 +502,22 @@ var bboxIntersect = function (shape1, shape2) {
 
 // check if a point is in our boundary
 var inBounds = function (x, y) {
-    return (boundary < x && x < width - boundary &&
-            boundary < y && y < height - boundary)
+    return inBoundsX(x) && inBoundsY(y);
+}
+
+// check the x of a point
+var inBoundsX = function (x) {
+    return between(boundary, x, width - boundary);
+}
+
+// check the y of a point
+var inBoundsY = function (y) {
+    return between(boundary, y, height - boundary);
+}
+
+// check if a value is between two other values
+var between= function (a, b, c) {
+    return (a < b && b < c);
 }
 
 // random number between min and max
@@ -447,10 +544,13 @@ var debug = function() {
 // INPUT PROCESSING
 // ----------------------------------------------------------------
 
+// the input processing triggers for when the game is active
 var setGameInput = function () {
 
     // KEYBOARD PROCESSING THINGS
     // --------------------------------
+
+    // when a key is pressed
     document.onkeydown = function (e) {
 
         // get the key
@@ -462,6 +562,7 @@ var setGameInput = function () {
         }
     }
 
+    // when a key is released
     document.onkeyup = function (e) {
 
         // get the key
@@ -477,6 +578,7 @@ var setGameInput = function () {
     // MOUSE PROCESSING THINGS
     // --------------------------------
 
+    // when the mouse is clicked
     document.onmousedown = function (e) {
 
         // update state
@@ -492,7 +594,7 @@ var setGameInput = function () {
             };
             add(mouseOn);
 
-            // for this loop, we've added the circle
+            // tell state that the circle was just added
             recent = true;
         }
 
@@ -503,6 +605,7 @@ var setGameInput = function () {
         //debug();
     }
 
+    // while the mouse is held down
     document.onmousemove = function (e) {
 
         // save old position
@@ -515,19 +618,20 @@ var setGameInput = function () {
 
         // if we're clicking on something...
         if (mouseDown) {
-            // moving condition
+            // if we're on a circle
             if (mouseOn) {
 
+                // tell state we've moved
                 moved = true;
 
-                // for each selected circle...
+                // move all the circles in our selection
                 selection.each(function () {
                     move(this,
                          cursorX - cursorXprev,
                          cursorY - cursorYprev);
                 });
             }
-            // if we're not mousing over anything...
+            // if we're not on a circle...
             else {
                 drawBox();
             }
@@ -536,6 +640,7 @@ var setGameInput = function () {
         //debug();
     }
 
+    // when the mouse is released
     document.onmouseup = function (e) {
 
         // if we made a box...
@@ -558,6 +663,7 @@ var setGameInput = function () {
             if (!shift && !moved) {
                 empty();
             }
+
             // if we're shift-clicking something selected, remove it
             if (shift && selected(mouseOn) && !moved && !recent) {
                 remove(mouseOn);
@@ -567,19 +673,12 @@ var setGameInput = function () {
                 add(mouseOn);
             }
         }
-        // if not, we can clear the selection
+        // if we clicked in space, empty the selection
         else {
             empty();
         }
 
-        success = true;
-        for (var i in crossed) {
-            if (crossed[i]) {
-                success = false;
-                break;
-            }
-        }
-        background.fill( success ? "#eeffee" : "#ffeeee" );
+        didWeWin();
 
         // update state
         mouseOn = false;
